4705366f740df8acde9aa9ad0ccd4bce2085902b
Parker Jones <parker.jones0211@gmail.com>
2020-06-08 19:49:24 -0400

    Initial commit.

---
+++ MonolithGuns/Gun.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MonolithGuns
+{
+    public class Gun
+    {
+        public string Name;
+        public string WeaponPart;
+        public int Crafting;
+        public int CopperBar;
+        public int IronBar;
+        public int SteelBar;
+        public int SilverBar;
+        public int GoldBar;
+        public int TitaniumBar;
+
+        public double CoalChunk => (CopperBar * 1d / 2d) + (IronBar * 3d / 4d) + ((SteelBar * (3d / 4d) * 3) + 3) + (SilverBar * 1d) + (GoldBar * 1d) + (TitaniumBar * 1d);
+        public double CopperChunk => CopperBar * 1.5;
+        public double IronChunk => (IronBar * 1.5) + 3 * (SteelBar * 1.5);
+        public double SilverChunk => SilverBar * 1.5;
+        public double GoldChunk => GoldBar * 1.5;
+        public double TitaniumChunk => TitaniumBar * 1.5;
+    }
+}
---
+++ MonolithGuns/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+
+/*
+ * Title: 
+ * Author: 
+ * Date: 
+ * Purpose: 
+ */
+
+namespace MonolithGuns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Gun Makarov = new Gun
+            {
+                Name = "Makarov",
+                WeaponPart = "Pistol",
+                Crafting = 5,
+                CopperBar = 8,
+                IronBar = 5
+            };
+            Gun M1911 = new Gun
+            {
+                Name = "M1911",
+                WeaponPart = "Pistol",
+                Crafting = 12,
+                CopperBar = 9,
+                IronBar = 6
+            };
+            Gun P99 = new Gun
+            {
+                Name = "P99",
+                WeaponPart = "Pistol",
+                Crafting = 15,
+                CopperBar = 8,
+                IronBar = 8
+            };
+            Gun FiveSeven = new Gun
+            {
+                Name = "FN-FiveSeven",
+                WeaponPart = "Pistol",
+                Crafting = 18,
+                CopperBar = 7,
+                IronBar = 9,
+                SteelBar = 2
+            };
+            Gun MAC11 = new Gun
+            {
+                Name = "Mac-11",
+                WeaponPart = "Pistol",
+                Crafting = 20,
+                CopperBar = 9,
+                IronBar = 10
+            };
+            Gun MR96 = new Gun
+            {
+                Name = "MR96",
+                WeaponPart = "Pistol",
+                Crafting = 20,
+                CopperBar = 8,
+                IronBar = 5,
+                SteelBar = 5
+            };
+            Gun MP5 = new Gun
+            {
+                Name = "HK MP5",
+                WeaponPart = "Pistol",
+                Crafting = 21,
+                CopperBar = 10,
+                IronBar = 12,
+                SteelBar = 2
+            };
+            Gun UMP = new Gun
+            {
+                Name = "H&K UMP .45",
+                WeaponPart = "Pistol",
+                Crafting = 27,
+                CopperBar = 14,
+                IronBar = 16
+            };
+            Gun AK = new Gun
+            {
+                Name = "AK-74",
+                WeaponPart = "Damaged Rifle",
+                Crafting = 28,
+                CopperBar = 11,
+                IronBar = 11,
+                SteelBar = 1
+            };
+            Gun Glock18 = new Gun
+            {
+                Name = "Glock 18",
+                WeaponPart = "Pistol",
+                Crafting = 28,
+                CopperBar = 8,
+                IronBar = 12,
+                SteelBar = 6
+            };
+            Gun AR15 = new Gun
+            {
+                Name = "AR-15",
+                WeaponPart = "Damaged Rifle",
+                Crafting = 30,
+                CopperBar = 11,
+                IronBar = 11,
+                SteelBar = 3
+            };
+            Gun Thompson = new Gun
+            {
+                Name = "Thompson",
+                WeaponPart = "Pistol",
+                Crafting = 31,
+                CopperBar = 15,
+                IronBar = 17,
+                SteelBar = 2
+            };
+            Gun Glock17 = new Gun
+            {
+                Name = "Glock 17",
+                WeaponPart = "Pistol",
+                Crafting = 32,
+                CopperBar = 8,
+                IronBar = 8,
+                SteelBar = 3,
+                SilverBar = 1,
+                GoldBar = 2
+            };
+            Gun Vector = new Gun
+            {
+                Name = "Kriss Vector",
+                WeaponPart = "Pistol",
+                Crafting = 34,
+                IronBar = 12,
+                SteelBar = 7,
+                SilverBar = 5,
+                GoldBar = 6
+            };
+            Gun Deagle = new Gun
+            {
+                Name = "Desert Eagle",
+
+        }
+    }
+}
